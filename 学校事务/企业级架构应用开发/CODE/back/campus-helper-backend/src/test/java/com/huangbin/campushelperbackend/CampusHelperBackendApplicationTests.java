fab91320594040d5a1838a71876e7b6aa8cf24df
HB0121 <281975492@qq.com>
2026-05-01 09:44:51 +0800

    vault backup: 2026-05-01 09:44:51

---
+++ 学校事务/企业级架构应用开发/CODE/back/campus-helper-backend/src/test/java/com/huangbin/campushelperbackend/CampusHelperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangbin.campushelperbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusHelperBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
