0914b48a555c3b14626ed461d986a7710b523e16
HB0121 <281975492@qq.com>
2026-05-01 09:14:39 +0800

    vault backup: 2026-05-01 09:14:39

--- 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/vite.config.js
+++ 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
+import { VantResolver } from 'unplugin-vue-components/resolvers'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // 配置 Vant 4 按需引入
+    Components({
+      resolvers: [VantResolver()],
+    }),
+  ],
 })
