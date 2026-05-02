6d151808573acb9028e7b4bf5e7308b2d5096601
HB0121 <281975492@qq.com>
2026-05-02 12:43:30 +0800

    vault backup: 2026-05-02 12:43:30

--- 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/src/App.vue
+++ 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import PublishPage from './views/PublishPage.vue';
 </script>
 
 <template>
-  <HelloWorld />
+  <PublishPage/>
 </template>
+
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  background-color: #f7f8fa; /* 配合 Vant 的底层灰色 */
+}
+</style>
