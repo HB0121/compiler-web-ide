b2179ac0ec5fdf47c36dd4f71b7c50cbfd09fa9f
HB0121 <281975492@qq.com>
2026-05-02 10:01:55 +0800

    vault backup: 2026-05-02 10:01:54

--- 学校事务/企业级架构应用开发/CODE/back/campus-helper-backend/src/main/java/com/huangbin/campushelperbackend/mapper/TaskMapper.java
+++ 学校事务/企业级架构应用开发/CODE/back/campus-helper-backend/src/main/java/com/huangbin/campushelperbackend/mapper/TaskMapper.java
@@ -2,6 +2,8 @@ package com.huangbin.campushelperbackend.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.huangbin.campushelperbackend.entity.Task;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface TaskMapper extends BaseMapper<Task> {
 }
