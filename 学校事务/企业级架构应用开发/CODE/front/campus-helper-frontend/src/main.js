a4430faf4ac5d61a1ce9b6843720774d97d54a8a
HB0121 <281975492@qq.com>
2026-05-05 06:52:30 +0800

    vault backup: 2026-05-05 06:52:30

--- 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/src/main.js
+++ 学校事务/企业级架构应用开发/CODE/front/campus-helper-frontend/src/main.js
@@ -1,15 +1,30 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { setupVant } from './plugins/vant' // 抽离的配置
+import { setupVant } from './plugins/vant'
+import axios from 'axios' // 引入 axios
+
+// ================== 新增：Axios 全局拦截器 ==================
+// 请求拦截器：在浏览器发出请求之前，自动执行这里的代码
+axios.interceptors.request.use(
+  config => {
+    // 从保险柜拿出门票
+    const token = localStorage.getItem('token')
+    if (token) {
+      // 如果有门票，就把它放进请求头 (Headers) 的 Authorization 字段里
+      // 这里的 'Bearer ' 是业界标准的 JWT 前缀，注意后面有个空格！
+      config.headers['Authorization'] = `Bearer ${token}`
+    }
+    return config
+  },
+  error => {
+    return Promise.reject(error)
+  }
+)
+// ==========================================================
 
 const app = createApp(App)
 
-// 1. 注册 Vant UI
 setupVant(app)
-
-// 2. 注册路由
 app.use(router)
-
-// 3. 挂载应用
 app.mount('#app')
