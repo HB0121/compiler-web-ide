2143d9c0453e650de4bf1f3d875f5942e958450d
HB0121 <281975492@qq.com>
2026-05-05 07:11:31 +0800

    vault backup: 2026-05-05 07:11:31

---
+++ 学校事务/企业级架构应用开发/CODE/back/campus-helper-backend/src/main/java/com/huangbin/campushelperbackend/entity/User.java
@@ -0,0 +1,22 @@
+package com.huangbin.campushelperbackend.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+@Data
+@TableName("users")
+public class User {
+    @TableId(type = IdType.AUTO)
+    private Long userId;
+
+    private String studentId; // 对应 student_id
+
+    private String nickname;  // 对应 nickname
+
+    private String passwordHash; // 对应 password_hash
+
+    private Integer creditScore; // 对应 credit_score
+}
