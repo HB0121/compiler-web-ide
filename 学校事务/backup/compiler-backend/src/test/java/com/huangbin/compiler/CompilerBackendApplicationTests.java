dbc27f706c7762297b5bdd23e324d46b815e2a3b
HB0121 <281975492@qq.com>
2026-05-12 10:37:55 +0800

    vault backup: 2026-05-12 10:37:55

---
+++ 学校事务/backup/compiler-backend/src/test/java/com/huangbin/compiler/CompilerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huangbin.compiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompilerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
