8d086df22f39befae1be805b9695a97e1e744975
HB0121 <281975492@qq.com>
2026-05-15 09:49:45 +0800

    vault backup: 2026-05-15 09:49:45

--- 学校事务/编译原理/课程设计/outputs/assembly.asm
+++ 学校事务/编译原理/课程设计/outputs/assembly.asm
@@ -17,7 +17,6 @@ data segment
   _msg_s db 0ah,'Input:',0
   next_row db 0dh,0ah,'$'
   error db 'input error, please re-enter: ','$'
-  limit dw 3
 data ends
 code segment
 start:
@@ -30,16 +29,15 @@ start:
     mov ax,data
     mov ds,ax
 
-add:
+square:
     PUSH BP
     MOV BP,SP
-    SUB SP,4
+    SUB SP,2
     MOV AX,ss:[bp+4]
-    ADD AX,ss:[bp+6]
+    MOV BX,ss:[bp+4]
+    MUL BX
     MOV ss:[bp-2],AX
     MOV AX,ss:[bp-2]
-    MOV ss:[bp-4],AX
-    MOV AX,ss:[bp-4]
     MOV SP,BP
     POP BP
     RET
@@ -47,33 +45,32 @@ add:
 main:
     PUSH BP
     MOV BP,SP
-    SUB SP,8
-    MOV AX,0
+    SUB SP,10
+    MOV AX,4
     MOV ss:[bp-2],AX
-    MOV AX,0
+    MOV AX,ss:[bp-2]
+    PUSH AX
+    CALL square
     MOV ss:[bp-4],AX
-_9:
-    MOV AX,ss:[bp-2]
-    CMP AX,limit
-    JL _11
-    JMP far ptr _18
-_11:
-    MOV AX,ss:[bp-2]
-    PUSH AX
     MOV AX,ss:[bp-4]
-    PUSH AX
-    CALL add
     MOV ss:[bp-6],AX
     MOV AX,ss:[bp-6]
-    MOV ss:[bp-4],AX
-    MOV AX,ss:[bp-2]
-    ADD AX,1
+    CMP AX,10
+    JG _11
+    JMP far ptr _14
+_11:
+    MOV AX,ss:[bp-6]
+    PUSH AX
+    CALL write
     MOV ss:[bp-8],AX
-    MOV AX,ss:[bp-8]
-    MOV ss:[bp-2],AX
-    JMP far ptr _9
-_18:
-    MOV AX,ss:[bp-4]
+    JMP far ptr _16
+_14:
+    MOV AX,0
+    PUSH AX
+    CALL write
+    MOV ss:[bp-10],AX
+_16:
+    MOV AX,0
     mov ah,4ch
     int 21h
 
--- 学校事务/编译原理/课程设计/outputs/llvm_ir.ll
+++ 学校事务/编译原理/课程设计/outputs/llvm_ir.ll
@@ -1,27 +1,26 @@
+@.fmt_int = private unnamed_addr constant [4 x i8] c"%d\0A\00"
+declare i32 @printf(i8*, ...)
+
 define i32 @main() {
 entry:
-  %i = alloca i32
-  %limit = alloca i32
-  %total = alloca i32
-  store i32 3, i32* %limit
-  store i32 0, i32* %i
-  store i32 0, i32* %total
-  br label %L9
-L9:
-  %v1 = load i32, i32* %i
-  %v2 = load i32, i32* %limit
-  %cmp1 = icmp slt i32 %v1, %v2
-  br i1 %cmp1, label %L11, label %L18
-L11:
-  %v3 = load i32, i32* %total
-  %v4 = load i32, i32* %i
+  %a = alloca i32
+  %b = alloca i32
+  store i32 4, i32* %a
+  %v1 = load i32, i32* %a
   %t2 = add i32 0, 0
-  store i32 %t2, i32* %total
-  %v5 = load i32, i32* %i
-  %t3 = add i32 %v5, 1
-  store i32 %t3, i32* %i
-  br label %L9
-L18:
-  %v6 = load i32, i32* %total
-  ret i32 %v6
+  store i32 %t2, i32* %b
+  %v2 = load i32, i32* %b
+  %cmp1 = icmp sgt i32 %v2, 10
+  br i1 %cmp1, label %L11, label %L14
+L11:
+  %v3 = load i32, i32* %b
+  call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.fmt_int, i32 0, i32 0), i32 %v3)
+  %t3 = add i32 0, 0
+  br label %L16
+L14:
+  call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.fmt_int, i32 0, i32 0), i32 0)
+  %t4 = add i32 0, 0
+  br label %L16
+L16:
+  ret i32 0
 }
