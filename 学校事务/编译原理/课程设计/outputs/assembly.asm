6978a891915779a508f9e33b4d4b2d738430a68b
HB0121 <281975492@qq.com>
2026-05-15 13:52:41 +0800

    vault backup: 2026-05-15 13:52:40

--- 学校事务/编译原理/课程设计/outputs/assembly.asm
+++ 学校事务/编译原理/课程设计/outputs/assembly.asm
@@ -29,33 +29,14 @@ start:
     mov ax,data
     mov ds,ax
 
-fn_factor:
+fn_square:
     PUSH BP
     MOV BP,SP
-    SUB SP,8
+    SUB SP,2
     MOV AX,ss:[bp+4]
-    CMP AX,1
-    JLE _3
-    JMP far ptr _5
-_3:
-    MOV AX,1
-    MOV ss:[bp-2],AX
-    JMP far ptr _10
-_5:
-    MOV AX,ss:[bp+4]
-    SUB AX,1
-    MOV ss:[bp-4],AX
-    MOV AX,ss:[bp-4]
-    PUSH AX
-    CALL fn_factor
-    MOV ss:[bp-6],AX
-    MOV AX,ss:[bp+4]
-    MOV BX,ss:[bp-6]
+    MOV BX,ss:[bp+4]
     MUL BX
-    MOV ss:[bp-8],AX
-    MOV AX,ss:[bp-8]
     MOV ss:[bp-2],AX
-_10:
     MOV AX,ss:[bp-2]
     MOV SP,BP
     POP BP
@@ -64,17 +45,31 @@ _10:
 main:
     PUSH BP
     MOV BP,SP
-    SUB SP,6
-    MOV AX,5
+    SUB SP,10
+    MOV AX,4
     MOV ss:[bp-2],AX
     MOV AX,ss:[bp-2]
     PUSH AX
-    CALL fn_factor
+    CALL fn_square
     MOV ss:[bp-4],AX
     MOV AX,ss:[bp-4]
+    MOV ss:[bp-6],AX
+    MOV AX,ss:[bp-6]
+    CMP AX,10
+    JG _11
+    JMP far ptr _14
+_11:
+    MOV AX,ss:[bp-6]
     PUSH AX
     CALL write
-    MOV ss:[bp-6],AX
+    MOV ss:[bp-8],AX
+    JMP far ptr _16
+_14:
+    MOV AX,0
+    PUSH AX
+    CALL write
+    MOV ss:[bp-10],AX
+_16:
     MOV AX,0
     mov ah,4ch
     int 21h
--- 学校事务/编译原理/课程设计/outputs/llvm_ir.ll
+++ 学校事务/编译原理/课程设计/outputs/llvm_ir.ll
@@ -3,11 +3,24 @@ declare i32 @printf(i8*, ...)
 
 define i32 @main() {
 entry:
-  %value = alloca i32
-  store i32 5, i32* %value
-  %v1 = load i32, i32* %value
+  %a = alloca i32
+  %b = alloca i32
+  store i32 4, i32* %a
+  %v1 = load i32, i32* %a
+  %t2 = add i32 0, 0
+  store i32 %t2, i32* %b
+  %v2 = load i32, i32* %b
+  %cmp1 = icmp sgt i32 %v2, 10
+  br i1 %cmp1, label %L11, label %L14
+L11:
+  %v3 = load i32, i32* %b
+  call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.fmt_int, i32 0, i32 0), i32 %v3)
+  %t3 = add i32 0, 0
+  br label %L16
+L14:
+  call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.fmt_int, i32 0, i32 0), i32 0)
   %t4 = add i32 0, 0
-  call i32 (i8*, ...) @printf(i8* getelementptr inbounds ([4 x i8], [4 x i8]* @.fmt_int, i32 0, i32 0), i32 %t4)
-  %t5 = add i32 0, 0
+  br label %L16
+L16:
   ret i32 0
 }
